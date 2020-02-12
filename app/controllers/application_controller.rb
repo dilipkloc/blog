eedc1caaa64fe75ada58b201519f6957ca2f160f
dilipkloc <dilipkloctech@gmail.com>
2020-02-13 00:48:54 +0530

    sign in with user name and email. Registration with gender, mobile and dob

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+  
+    def configure_permitted_parameters
+  
+      added_attrs = [:username, :email, :password, :password_confirmation, :remember_me,:mobile,:gender,:dob]
+  
+      devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
+  
+      devise_parameter_sanitizer.permit(:sign_in, 
+        keys: [:login, :password, :password_confirmation])
+  
+      devise_parameter_sanitizer.permit( :account_update, keys: added_attrs )
+  
+      # devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      #   user_params.permit(:username, :email)
+      # end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   get 'categories', to: 'categories#index', as: 'categories'
   get 'categories/new', to: 'categories#new'
   get 'categories/:id', to: 'categories#show', as: 'category'
