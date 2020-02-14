eb03ef4b0c9fb63a8b79cfc67f2952cd59a868b9
dilipkloc <dilipkloctech@gmail.com>
2020-02-15 00:54:34 +0530

    feat: association to comments, user and article

--- app/models/postcomment.rb
+++ app/models/postcomment.rb
@@ -1,3 +1,4 @@
 class Postcomment < ApplicationRecord
   belongs_to :article
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :permissions   
   has_many :roles, through: :permissions
   has_many :articles
+  has_many :postcomments
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
